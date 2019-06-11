dbc4bd7d30e6f9a33730a8f0f7ae70274bab9d0f
brindalakshmi <brindalakshmim@gmail.com>
2019-06-11 13:22:58 -0500

    complete

---
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,25 @@
+package org.launchcode.hellospring.controllers;
+
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("hello")
+public class HelloController {
+    @RequestMapping(value = "")
+    @ResponseBody
+    public String index(HttpServletRequest request){
+
+        String name = request.getParameter("name");
+
+        return "Hello " + name;
+    }
+
+    @RequestMapping(value = "goodbye")
+    @ResponseBody
+    public String goodbye() {
+        return "Goodbye";
+    }
+}
